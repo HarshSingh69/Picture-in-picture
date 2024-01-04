5b6873363fe6ef4e547b04f2e2da625f73cdde14
HarshSingh69 <harshsingh22440@gmail.com>
2024-01-04 17:46:20 +0530

    final Updatation

---
+++ script.js
@@ -0,0 +1,25 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+async function selectMediaStream(){
+    try{
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () =>{
+            videoElement.play();
+        }
+    } catch(error){
+        console.log('Error here:',error)
+    }
+}
+
+button.addEventListener('click',async () =>{
+    // disable button
+    button.disabled = true;
+    // start picture in picture
+    await videoElement.requestPictureInPicture();
+    // reset button
+    button.disabled = false;
+});
+
+selectMediaStream();
